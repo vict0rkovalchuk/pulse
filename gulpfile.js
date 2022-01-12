5fbd98d7610235416bdfb3f58242f66d47181b1c
Victor <vict02r05@gmail.com>
2022-01-12 12:13:07 +0200

    uninstall gulp-gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-const ghPages = require('gulp-gh-pages');
 
 // Static server
 gulp.task('server', function () {
@@ -34,10 +33,6 @@ gulp.task('styles', function () {
 gulp.task('watch', function () {
   gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
   gulp.watch('src/*.html').on('change', browserSync.reload);
-});
-
-gulp.task('deploy', function () {
-  return gulp.src('/src/**/*').pipe(ghPages());
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
