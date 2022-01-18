083ad9135a208021fc7efd39243a8c4e3299be2e
Victor <vict02r05@gmail.com>
2022-01-18 13:50:14 +0200

    validate form and adapt one

--- src/js/script.js
+++ src/js/script.js
@@ -62,4 +62,34 @@ $(document).ready(function () {
       $('.overlay, #order').fadeIn('fast');
     });
   });
+
+  function validateForms(form) {
+    return $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: 'required',
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: 'Пожалуйста введите свое имя',
+          minlength: jQuery.validator.format('Введите минимум {0} символа')
+        },
+        phone: 'Пожалуйста введите свой номер телефона',
+        email: {
+          required: 'Пожалуйста введите свою почту',
+          email: 'Формат почты должен быть name@domain.com'
+        }
+      }
+    });
+  }
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
 });
