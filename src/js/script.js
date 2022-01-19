1df40f5a8e503cd8a2b15b5df7a1172871e9aa79
Victor <vict02r05@gmail.com>
2022-01-19 14:24:18 +0200

    smooth scroll pageup element

--- src/js/script.js
+++ src/js/script.js
@@ -115,4 +115,22 @@ $(document).ready(function () {
     });
     return false;
   });
+
+  // pageup
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn('fast');
+    } else {
+      $('.pageup').fadeOut('slow');
+    }
+  });
+
+  // Smoothly jQuery variant
+
+  // $('a[href^="#"]').click(function () {
+  //   const _href = $(this).attr('href');
+  //   $('html, body').animate({ scrollTop: $(_href).offset().top + 'px' });
+  //   return false;
+  // });
 });
